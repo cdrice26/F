e2b2ef9eba38cf0d8ebf72a18e6b7e4564dd2d16
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-12-13 10:09:43 -0500

    Moved helper functions to helper package

--- cmd/list.go
+++ cmd/list.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"f/helper"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -9,45 +10,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func getDirectoryFromArgs(args []string) (string, error) {
-	if len(args) == 0 {
-		return os.Getwd()
-	} else {
-		return args[0], nil
-	}
-}
-
-func formatSize(size int64) string {
-	if size >= 1024*1024*1024 {
-		return fmt.Sprintf("%.2f GB", float64(size)/float64(1024*1024*1024))
-	} else if size >= 1024*1024 {
-		return fmt.Sprintf("%.2f MB", float64(size)/float64(1024*1024))
-	} else if size >= 1024 {
-		return fmt.Sprintf("%.2f KB", float64(size)/float64(1024))
-	}
-	return fmt.Sprintf("%d Bytes", size)
-}
-
-// Calculate total size of a directory (recursively)
-func getDirSize(path string) (int64, error) {
-	var totalSize int64
-	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			totalSize += info.Size()
-		}
-		return nil
-	})
-
-	return totalSize, err
-}
-
 func runList(cmd *cobra.Command, args []string) {
 
 	// Read the files from the directory
-	dir, err := getDirectoryFromArgs(args)
+	dir, err := helper.GetDirectoryFromArgs(args)
 	if err != nil {
 		fmt.Println("Error getting directory:", err)
 		return
@@ -84,15 +50,15 @@ func runList(cmd *cobra.Command, args []string) {
 		fileSize := ""
 		if file.IsDir() {
 			fileType = "Directory"
-			dirSize, err := getDirSize(filepath.Join(dir, file.Name()))
+			dirSize, err := helper.GetDirSize(filepath.Join(dir, file.Name()))
 			if err != nil {
 				fileSize = "Unknown"
 			} else {
-				fileSize = formatSize(dirSize)
+				fileSize = helper.FormatSize(dirSize)
 			}
 		} else {
 			fileType = "File"
-			fileSize = formatSize(fileInfo.Size())
+			fileSize = helper.FormatSize(fileInfo.Size())
 		}
 
 		// Displaying file metadata
